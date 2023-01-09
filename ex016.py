a8d89e12534cc4d0ebe76e6fae840186f47b45a0
Marcos <marcssis87@gmail.com>
2023-01-09 07:55:01 -0300

    Inicial Commit

--- ex016.py
+++ ex016.py
@@ -1,7 +1,7 @@
-import math
+from math import trunc
 numero = float(input('Digite um número real: '))
-inteiro = math.floor(numero)
-print('O número real digitado foi {} e sua porção inteira é: {}'.format(numero, inteiro))
+inteiro = trunc(numero)
+print('O número real digitado foi {} e sua porção inteira é: {}'.format(numero, trunc(numero)))
 
 '''num = float(input('Digite um valor: '))
 print('O valor digitado foi {} e sua porção é {}')'''
