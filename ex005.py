5e8f8a81f99899c8ca3626e5829ffa7b135236be
Marcos <marcssis87@gmail.com>
2023-01-09 10:29:11 -0300

    Inicial Commit

--- ex005.py
+++ ex005.py
@@ -2,3 +2,7 @@ n = int(input('Digite um número e você verá o antecessor e o sucessor dele: '
 ant = n-1
 suc = n+1
 print('Você escolheu o número {}, o antecessor dele é {} e o sucessor é {}.'.format(n, ant, suc))
+
+
+'''n = int(input('Digite um número e você verá o antecessor e o sucessor dele: '))n = int(input('Digite um número e você verá o antecessor e o sucessor dele:'))
+print('Você escolheu o número {}, o antecessor dele é {} e o sucessor é {}.'.format(n, (n-1), (n+1)))'''
--- ex006.py
+++ ex006.py
@@ -1,8 +1,16 @@
-import math
+from math import sqrt
+
+'''import math
 n = float(input('Digite um número e veja seu dobro, triplo e sua raiz quadrada: '))
 dobro = n*2
 triplo = n*3
 raiz = math.sqrt(n)
 
-print('Você escolheu o número {}, seu dobro é {}, seu triplo é {} e sua raiz quadrada é {}.'.format(n, dobro, triplo, raiz))
+print('Você escolheu o número {}, seu dobro é {}, seu triplo é {} e sua raiz é {}.'.format(n, dobro, triplo, raiz))'''
 
+n = int(input('Digite um número e veja seu dobro, triplo  raiz quadrada: '))
+print('Você digitou o número {}, seu dobro é {}, seu triplo é {} e a raiz é {:.2f}'.format(n, (n*2), (n*3), sqrt(n)))
+
+'''RAIZ QUADRADA
+n ** (1/2)
+'''
