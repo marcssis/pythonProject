51b6d87bbbb4cc6db38c1b310e0116e587b205ee
Marcos <marcssis87@gmail.com>
2023-01-10 00:34:38 -0300

    Inicial Commit

--- ex010.py
+++ ex010.py
@@ -1,5 +1,5 @@
 real = float(input('Digite quanto você tem na carteira: R$ '))
-dolar = real/3.27
+dolar = real/5.26
 
 print('Você tem R${:.2f} na carteira, sendo assim, pode comprar US${:.2f} em dolar.'.format(real, dolar))
 
--- ex011.py
+++ ex011.py
@@ -2,5 +2,5 @@ largura = float(input('Digite a largura da parede: '))
 altura = float(input('Digite da altura da parede: '))
 area = largura * altura
 qtd_tinta = area / 2
-print('A área da parede é {} metros quadrados e são necessários {} litros de tinta para pinta-la. '.format(area, qtd_tinta))
+print('A área da parede é de {}m² quadrados e são necessários {} litros de tinta para pinta-la. '.format(area, qtd_tinta))
 
